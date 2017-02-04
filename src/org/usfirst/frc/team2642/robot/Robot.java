1207aee0270877ffcb1e6a2f107d5d572067841a
Christian Colglazier <aquamorphcomputing@gmail.com>
2017-02-04 15:00:59 -0500

    Added vision tacking from prototypes

--- src/org/usfirst/frc/team2642/robot/Robot.java
+++ src/org/usfirst/frc/team2642/robot/Robot.java
@@ -9,8 +9,10 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.vision.VisionThread;
 
-import org.usfirst.frc.team2642.robot.commands.*;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team2642.robot.subsystems.*;
 
 /**
@@ -29,7 +31,24 @@ public class Robot extends IterativeRobot {
 	public static UsbCamera cameraGear;
 	private static final int IMG_WIDTH = 320;
 	private static final int IMG_HEIGHT = 240;
+	
+	private VisionThread gearVisionThread;
+	public static final Object gearImgLock = new Object();
+	public static double gearCenterXL = 10000.0;
+	public static double gearCenterYL = 10000.0;
+	public static double gearTargetAreaL = 100000.0;
+	public static double gearCenterXR = 100000.0;
+	public static double gearCenterYR = 100000.0;
+	public static double gearTargetAreaR = 100000.0;
+	public static double gearCenterX = 100000.0;
+	public static double gearCenterY = 100000.0;
 
+	private VisionThread boilerVisionThread;
+	public static final Object boilerImgLock = new Object();
+	public static double boilerCenterX = 0.0;
+	public static double boilerCenterY = 0.0;
+	public static double boilerCenterTargetArea = 0.0;
+	
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
 
@@ -49,6 +68,45 @@ public class Robot extends IterativeRobot {
 		setCameraBoilerVision(false);
 		setCameraGearVision(false);
 		
+		gearVisionThread = new VisionThread(cameraGear, new GearPipeline(),pegpipeline -> {
+			if(pegpipeline.filterContoursOutput().size() >= 2){
+				Rect gearRecL = Imgproc.boundingRect(pegpipeline.filterContoursOutput().get(0));
+				Rect gearRecR = Imgproc.boundingRect(pegpipeline.filterContoursOutput().get(1));
+				synchronized (gearImgLock) {
+					gearCenterXL = 2*gearRecL.x + gearRecL.width - (IMG_WIDTH/2);
+					gearCenterYL = 2*gearRecL.y + gearRecL.height - (IMG_HEIGHT/2);
+					gearTargetAreaL = gearRecL.area();
+					gearCenterXR = 2*gearRecR.x + gearRecR.width - (IMG_WIDTH/2);
+					gearCenterYR = 2*gearRecR.y + gearRecR.height - (IMG_HEIGHT/2);
+					gearTargetAreaR = gearRecR.area();
+					gearCenterX = gearCenterXL + gearCenterXR;
+					gearCenterY = gearCenterYL + gearCenterYR;
+				}
+			} else {
+				gearCenterXL = 10000.0;
+				gearCenterYL = 10000.0;
+				gearTargetAreaL = 10000.0;
+				gearCenterXR = 10000.0;
+				gearCenterYR = 10000.0;
+				gearTargetAreaR = 10000.0;
+				gearCenterX = 10000.0;
+				gearCenterY = 10000.0;
+			}
+		});
+		gearVisionThread.start();
+		
+		boilerVisionThread = new VisionThread(cameraBoiler, new BoilerPipeline(), pipeline -> {
+	        if (!pipeline.filterContoursOutput().isEmpty()) {
+	            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+	            synchronized (boilerImgLock) {
+	                boilerCenterX = 2*r.x + r.width - (IMG_WIDTH/2);
+	                boilerCenterY = 2*r.y + r.height - (IMG_HEIGHT/2);
+	                boilerCenterTargetArea = r.area();
+	            }
+	        }
+	    });
+		boilerVisionThread.start();
+		
 		//chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
@@ -60,16 +118,16 @@ public class Robot extends IterativeRobot {
 			cameraBoiler.setExposureManual(0);
 		} else {
 			cameraBoiler.setBrightness(30);
-			cameraBoiler.setExposureManual(30);
+			cameraBoiler.setExposureManual(35);
 		}
 	}
 	
 	public static void setCameraGearVision(boolean enabled) {
 		if(enabled) {
-			cameraGear.setBrightness(0);
+			cameraGear.setBrightness(30);
 			cameraGear.setExposureManual(0);
 		} else {
-			cameraGear.setBrightness(20);
+			cameraGear.setBrightness(10);
 			cameraGear.setExposureManual(20);
 		}
 	}
@@ -140,6 +198,11 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		
+		SmartDashboard.putNumber("Boiler Center X", boilerCenterX);
+		SmartDashboard.putNumber("Boiler Center Y", boilerCenterY);
+		SmartDashboard.putNumber("Gear Center X", gearCenterX);
+		SmartDashboard.putNumber("Gear Center Y", gearCenterY);
 	}
 
 	/**
